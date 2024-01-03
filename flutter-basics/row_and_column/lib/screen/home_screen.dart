e7a7290f4eb4f361d8557b686a7c44f9505867a0
jjinheeWorld <wlsgml450@naver.com>
2024-01-04 01:17:15 +0900

    세팅하기

---
+++ flutter-basics/row_and_column/lib/screen/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              // Container는 child 안에 들어간 위젯에 따라서 형태가 바뀌기도 하지만 Container에 직접 사이즈를 정할 수 있음.
+              Container(
+                color: Colors.red,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.orange,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.yellow,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.green,
+                width: 50.0,
+                height: 50.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
