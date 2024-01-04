61f4e2bb4256efcf3d1851497013fc8a4e8b85d9
jjinheeWorld <wlsgml450@naver.com>
2024-01-05 00:54:28 +0900

    crossAxisAlignment

--- flutter-basics/row_and_column/lib/screen/home_screen.dart
+++ flutter-basics/row_and_column/lib/screen/home_screen.dart
@@ -10,6 +10,9 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
+          height: MediaQuery.of(context).size.height,
+          // Row, Column 모두 주축에서는 최대한 차지할 수 있을 만큼 모두 차지함.
+          // 반대축은 최소한의 사이즈만큼만 기본적으로 차지함.
           child: Row(
             // MainAxisAlignment - 주축 정렬
             // start - 시작
@@ -18,7 +21,14 @@ class HomeScreen extends StatelessWidget {
             // spaceBetween - 위젯과 위젯의 사이가 동일하게 배치된다.
             // spaceEvenly - 위젯을 같은 간격으로 배치하지만 끝과 끝에도 위젯이 아닌 빈 간격으로 시작한다.
             // spaceAround - spaceEvenly + 끝과 끝의 간격을 1/2
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.start,
+            // CrossAxisAlignment - 반대축 정렬
+            // start - 시작
+            // end - 끝
+            // center - 가운데
+            // stretch - 최대한으로 늘린다.
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //
             children: [
               // Container는 child 안에 들어간 위젯에 따라서 형태가 바뀌기도 하지만 Container에 직접 사이즈를 정할 수 있음.
               Container(
