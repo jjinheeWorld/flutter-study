8b9b8dc595d665816d2e50ff315447566f3a1c64
jjinheeWorld <wlsgml450@naver.com>
2024-01-05 00:30:48 +0900

    MainAxisAlignment

--- flutter-basics/row_and_column/lib/screen/home_screen.dart
+++ flutter-basics/row_and_column/lib/screen/home_screen.dart
@@ -10,7 +10,15 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
-          child: Column(
+          child: Row(
+            // MainAxisAlignment - 주축 정렬
+            // start - 시작
+            // end - 끝
+            // center - 가운데
+            // spaceBetween - 위젯과 위젯의 사이가 동일하게 배치된다.
+            // spaceEvenly - 위젯을 같은 간격으로 배치하지만 끝과 끝에도 위젯이 아닌 빈 간격으로 시작한다.
+            // spaceAround - spaceEvenly + 끝과 끝의 간격을 1/2
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               // Container는 child 안에 들어간 위젯에 따라서 형태가 바뀌기도 하지만 Container에 직접 사이즈를 정할 수 있음.
               Container(
