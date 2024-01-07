70340d59d91d73c2daf4398a29807c592aaff93b
jjinheeWorld <wlsgml450@naver.com>
2024-01-08 02:41:09 +0900

    웹사이트 웹뷰에 띄우기

--- project/web_view/lib/screen/home_screen.dart
+++ project/web_view/lib/screen/home_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://github.com/jjinheeWorld');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController();
+  /// final result = WebViewController().loadRequest();
+  ///
+  /// final controller = WebViewController();
+  /// final result = controller.loadRequest();
+  ///
+  /// ..은 함수가 실행된 대상이 반환 + 함수 실행도 된 것
+  /// final result2 = WebViewController..loadRequest();
+  ///
+  /// final result2 = WebViewController();
+  /// result2.loadRequest();
+  WebViewController controller = WebViewController()..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
