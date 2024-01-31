0826f06ec1efd4612ba21e4441d2799bf073cedd
jjinheeWorld <wlsgml450@naver.com>
2024-01-31 23:18:11 +0900

    Slider 위젯 사용하기

--- project/random_number_generator/lib/screen/settings_screen.dart
+++ project/random_number_generator/lib/screen/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -9,11 +10,54 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Settings Screen'),
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                  children: maxNumber
+                      .toInt()
+                      .toString()
+                      .split('')
+                      .map(
+                        (e) => Image.asset(
+                          'asset/img/$e.png',
+                          width: 50.0,
+                          height: 70.0,
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              Slider(
+                value: maxNumber,
+                min: 10000,
+                max: 1000000,
+                onChanged: (double val) {
+                  setState(() {
+                    maxNumber = val;
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: RED_COLOR,
+                ),
+                child: Text('저장!'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
